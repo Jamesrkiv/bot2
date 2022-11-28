da6ca36d64d35c8f0c2f5900e6ce17d86118f55e
Jamesrkiv <kentja18@students.ecu.edu>
2022-11-28 17:33:06 -0500

    More function frameworking, added npm dependency

--- blackjack.js
+++ blackjack.js
@@ -1,12 +1,13 @@
 // DISCORD BOT 2
 // BLACKJACK FUNCTIONS
 // Created: 		11/12/22
-// Last modified:	11/25/22
+// Last modified:	11/28/22
 
 
 /* Node modules */
 const Discord = require('discord.js');                                                                  // Discord JS
 const Jimp = require('jimp');                                                                           // File manipulation                
+const JoinImages = require('join-images');                                                              // Image joining
 
 /* External JSONs */
 const config = require('./jsons/config.json'); 															// Generic config declaration in event other things are added
@@ -35,7 +36,6 @@ async function playBlackjack(msg, args, client, bet)
 
     await thread.join();
     await thread.members.add(msg.author.id);
-    // await thread.send("Test message!");
 
     // Probably a better way to do this, however, fuck you
     var spade   = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'j', 'q', 'k', 'a']; 
@@ -43,12 +43,30 @@ async function playBlackjack(msg, args, client, bet)
     var diamond = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'j', 'q', 'k', 'a'];
     var heart   = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'j', 'q', 'k', 'a'];
 
+    var pHand = [];                                                                                     // Player hand
+    var bHand = [];                                                                                     // Bot hand
+
     var cancelled = false;
     var ended = false;
     var reminders = 0;
 
-    const filter = m => m.author.tag === msg.author.tag;                                                // Filters messages in thread to original author
+    // First deal
+    bHand.push(drawCard(spade, club, diamond, heart));                                                  // Initial bot hand
+    bHand.push(drawCard(spade, club, diamond, heart));
+    pHand.push(drawCard(spade, club, diamond, heart));                                                  // Initial player hand
+    pHand.push(drawCard(spade, club, diamond, heart));
 
+    // TODO: Get hand as single image using handImage
+
+    var fBotHand = new Discord.MessageEmbed()
+        .setColor(0x934459)
+        .setTitle('Curiosity\'s Hand');
+
+    var fPlayerHand = new Discord.MessageEmbed()
+        .setColor(0x7C9759)
+        .setTitle('Your Hand');
+
+    const filter = m => m.author.tag === msg.author.tag;                                                // Filters messages in thread to original author
     // Needs to be able to get messages after x amount of time/messages
     // then parse the given responses for a valid reply--looping back
     // around if a valid response isn't given.
@@ -74,8 +92,6 @@ async function playBlackjack(msg, args, client, bet)
     }
     while(!ended);
 
-    await thread.delete();
-    
     // IMPORTANT NOTE !!!
     //
     // Need to re-check balance prior to any resolution, as the game could
@@ -90,8 +106,12 @@ async function playBlackjack(msg, args, client, bet)
         // TODO: Code to eat money here
         // (Prevents cheating if losing)
 
+        await thread.delete();
         return msg.reply("Timeout/error reached, ending game...");
     }
+
+    await thread.setArchived(true);
+    await thread.setLocked(true);
     
     return msg.reply("Game finished! (Placeholder)");
 }
@@ -110,8 +130,66 @@ async function playBlackjack(msg, args, client, bet)
 /*CARD FUNCTIONS*/
 
 
-// use Node Jimp to add value to card(s) and resize
-//
+// TODO: use Node Jimp to add value to card(s) and resize
+function genCard(card)
+{
+    return;
+}
+
+// TODO: use Node join-images to get imaged from genCard and output a single image
+function handImage()
+{
+    // JoinImages.joinImages()
+    return;
+}
+
+// Gets a card from the available options
+function drawCard(spade, club, diamond, heart)
+{
+    var selectedCard = [];
+    var num = randFromZero(3);
+    switch (num)
+    {
+        case 0:
+            selectedCard[0] = "spade";
+            var indx = randFromZero(spade.length);
+            selectedCard[1] = spade[indx];
+            spade.splice(indx, 1);
+            break;
+        case 1:
+            selectedCard[0] = "club";
+            var indx = randFromZero(club.length);
+            selectedCard[1] = club[indx];
+            club.splice(indx, 1);
+            break;
+        case 2:
+            selectedCard[0] = "diamond";
+            var indx = randFromZero(diamond.length);
+            selectedCard[1] = diamond[indx];
+            diamond.splice(indx, 1);
+            break;
+        case 3:
+            selectedCard[0] = "heart";
+            var indx = randFromZero(heart.length);
+            selectedCard[1] = heart[indx];
+            heart.splice(indx, 1);
+            break;
+    }
+    return selectedCard;
+}
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/*MISC FUNCTIONS*/
+
+
+function randFromZero(max)
+{
+    var num = Math.round(Math.random() * max);
+    if (num > max) num = max;
+    return num;
+}
 
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
